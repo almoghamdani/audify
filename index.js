efc15bcdad0bf04ea80e05309fac3bd3d0abd09b
Almog Hamdani <almogh52@gmail.com>
2019-12-16 22:08:19 +0200

    feat: add implementation of all enums

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-exports = module.exports = require("bindings")("audify");
+const rawAudify = require("bindings")("audify");
+
+const OpusApplication = {
+	OPUS_APPLICATION_VOIP: 2048,
+	OPUS_APPLICATION_AUDIO: 2049,
+	OPUS_APPLICATION_RESTRICTED_LOWDELAY: 2051
+};
+
+const RtAudioApi = {
+	UNSPECIFIED: 0,
+	LINUX_ALSA: 1,
+	LINUX_PULSE: 2,
+	LINUX_OSS: 3,
+	UNIX_JACK: 4,
+	MACOSX_CORE: 5,
+	WINDOWS_WASAPI: 6,
+	WINDOWS_ASIO: 7,
+	WINDOWS_DS: 8,
+	RTAUDIO_DUMMY: 9
+};
+
+const RtAudioFormat = {
+	RTAUDIO_SINT8: 0x1,
+	RTAUDIO_SINT16: 0x2,
+	RTAUDIO_SINT24: 0x4,
+	RTAUDIO_SINT32: 0x8,
+	RTAUDIO_FLOAT32: 0x10,
+	RTAUDIO_FLOAT64: 0x20
+};
+
+const RtAudioStreamFlags = {
+	RTAUDIO_NONINTERLEAVED: 0x1,
+	RTAUDIO_MINIMIZE_LATENCY: 0x2,
+	RTAUDIO_HOG_DEVICE: 0x4,
+	RTAUDIO_SCHEDULE_REALTIME: 0x8,
+	RTAUDIO_ALSA_USE_DEFAULT: 0x10,
+	RTAUDIO_JACK_DONT_CONNECT: 0x20
+};
+
+exports = module.exports = { ...rawAudify, OpusApplication, RtAudioApi, RtAudioFormat, RtAudioStreamFlags };
