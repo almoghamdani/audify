9346d5fcec9a012afa0e71be991c25e695c8140b
Almog Hamdani <almogh52@gmail.com>
2023-01-14 15:40:02 +0200

    fix: changes to accommodate RtAudio's breaking changes + reformatting

--- src/audify.cpp
+++ src/audify.cpp
@@ -4,19 +4,24 @@
 #include "opus_encoder.h"
 #include "rt_audio.h"
 
-Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  OpusEncoderWrap::Init(env, exports);
-  OpusDecoderWrap::Init(env, exports);
-  RtAudioWrap::Init(env, exports);
-  
-  const napi_status add_cleanup_hook_status = napi_add_env_cleanup_hook(env, [](void*) {
-    OpusEncoderWrap::Destroy();
-    OpusDecoderWrap::Destroy();
-    RtAudioWrap::Destroy();
-  }, nullptr);
-  NAPI_THROW_IF_FAILED_VOID(env, add_cleanup_hook_status);
-  
-  return exports;
+Napi::Object Init(Napi::Env env, Napi::Object exports)
+{
+    OpusEncoderWrap::Init(env, exports);
+    OpusDecoderWrap::Init(env, exports);
+    RtAudioWrap::Init(env, exports);
+
+    const napi_status add_cleanup_hook_status = napi_add_env_cleanup_hook(
+        env,
+        [](void*)
+        {
+            OpusEncoderWrap::Destroy();
+            OpusDecoderWrap::Destroy();
+            RtAudioWrap::Destroy();
+        },
+        nullptr);
+    NAPI_THROW_IF_FAILED_VOID(env, add_cleanup_hook_status);
+
+    return exports;
 }
 
 NODE_API_MODULE(audify, Init)
--- src/opus_encoder.h
+++ src/opus_encoder.h
@@ -5,27 +5,28 @@
 
 #define MAX_DATA_SIZE 2000
 
-class OpusEncoderWrap : public Napi::ObjectWrap<OpusEncoderWrap> {
- public:
-  static Napi::Object Init(Napi::Env env, Napi::Object exports);
-  static void Destroy();
+class OpusEncoderWrap : public Napi::ObjectWrap<OpusEncoderWrap>
+{
+public:
+    static Napi::Object Init(Napi::Env env, Napi::Object exports);
+    static void Destroy();
 
- public:
-  OpusEncoderWrap(const Napi::CallbackInfo &info);
-  ~OpusEncoderWrap();
+public:
+    OpusEncoderWrap(const Napi::CallbackInfo& info);
+    ~OpusEncoderWrap() override;
 
-  Napi::Value encode(const Napi::CallbackInfo &info);
-  Napi::Value encodeFloat(const Napi::CallbackInfo &info);
+    Napi::Value encode(const Napi::CallbackInfo& info);
+    Napi::Value encodeFloat(const Napi::CallbackInfo& info);
 
-  void setBitrate(const Napi::CallbackInfo &info, const Napi::Value &value);
-  Napi::Value getBitrate(const Napi::CallbackInfo &info);
+    void setBitrate(const Napi::CallbackInfo& info, const Napi::Value& value);
+    Napi::Value getBitrate(const Napi::CallbackInfo& info);
 
- private:
-  inline static Napi::FunctionReference constructor;
+private:
+    inline static Napi::FunctionReference constructor;
 
-  std::string getErrorMsg(int error);
+    std::string getErrorMsg(int error);
 
-  OpusEncoder *_opusEncoder;
-  int _opus_error;
-  int _channels;
+    OpusEncoder* _opusEncoder;
+    int _opus_error;
+    int _channels;
 };
